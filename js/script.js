540aa1359d9f624b13952bcf94f7e992112d90d5
ooo3289 <ooo3289@naver.com>
2021-07-31 18:10:19 +0900

    일단저장

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 window.onload = function(){
     var swiper = new Swiper(".main_slide", {
         loop: true,
+        slidesPerView: 1.8,
+        speed:4000,
+        // autoplay: {
+        //   delay: 0,
+        //   disableOnInteraction: false,
+        // },
+        spaceBetween:30,
+        centeredSlides:true,
         navigation: {
           nextEl: ".main_slide .arr_next",
           prevEl: ".main_slide .arr_prev",
